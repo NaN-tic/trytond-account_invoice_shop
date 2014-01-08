05f3daefaa3d56f35e6c1ed6dbd4ad0d247937dd
Albert Cervera i Areny <albert@nan-tic.com>
2014-01-08 01:11:34 +0100

    Make shop field required and void complex code on create() and write().

--- invoice.py
+++ invoice.py
@@ -12,55 +12,10 @@ __metaclass__ = PoolMeta
 
 class Invoice:
     __name__ = 'account.invoice'
-
-    shop = fields.Many2One('sale.shop', 'Shop',
-        states={'required': Eval('state') != 'draft'},
-        select=True, depends=['state'])
-
-    @classmethod
-    def __setup__(cls):
-        super(Invoice, cls).__setup__()
-        cls._error_messages.update({
-                'not_invoice_shop': 'What shop would like to sell? '
-                    'Go to preferences',
-                'invoice_not_shop': 'Invoice have not related a shop',
-                'edit_invoice_by_shop': 'You cannot edit this invoice '
-                    'because you do not have permission to edit in this shop.',
-        })
+    shop = fields.Many2One('sale.shop', 'Shop', required=True)
 
     @staticmethod
     def default_shop():
         User = Pool().get('res.user')
         user = User(Transaction().user)
         return user.shop.id if user.shop else None
-
-    @classmethod
-    def create(cls, vlist):
-        for vals in vlist:
-            User = Pool().get('res.user')
-            user = User(Transaction().user)
-
-            if not user.id == 0 and not user.shop:
-                cls.raise_user_error('not_sale_shop')
-
-            vals = vals.copy()
-            if not 'shop' in vals:
-                vals['shop'] = user.shop.id
-        return super(Invoice, cls).create(vlist)
-
-    @classmethod
-    def write(cls, invoices, vals):
-        '''
-        Only edit Invoice users available edit in this shop
-        '''
-        User = Pool().get('res.user')
-        user = User(Transaction().user)
-
-        if not user.id == 0:
-            shops = [s.id for s in user.shops]
-            for invoice in invoices:
-                if not invoice.shop:
-                    cls.raise_user_error('invoice_not_shop')
-                if not invoice.shop.id in shops:
-                    cls.raise_user_error('edit_invoice_by_shop')
-        super(Invoice, cls).write(invoices, vals)
