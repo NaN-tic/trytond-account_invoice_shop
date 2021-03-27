0789375b91827d9a479b78a96cbe02661014367e
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-27 23:46:02 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
-
+from . import invoice
 
 def register():
     Pool.register(
-        Sale,
-        Invoice,
+        invoice.Sale,
+        invoice.Invoice,
         module='account_invoice_shop', type_='model')
--- invoice.py
+++ invoice.py
@@ -7,8 +7,6 @@ from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 
-__all__ = ['Sale', 'Invoice']
-
 
 class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
